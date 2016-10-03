ea983f5104663d8f08aed556ee257a6c14451696
Pamela Lim <pamelaliusm@gmail.com>
2016-10-03 11:46:49 +1000

    algo all done

--- app/Http/Controllers/LoadController.php
+++ app/Http/Controllers/LoadController.php
@@ -34,7 +34,14 @@ class LoadController extends Controller
 
     public function loadquestions ()
     {
-        Excel::selectSheets('imp1')->load('public/questions.xlsx', function ($reader) {
+        Excel::selectSheets('testing')->load('public/questions.xlsx', function ($reader) {
+            $questions = $reader->all();
+            foreach ($questions as $question) {
+                \App\Question::create($question->toArray());
+            }
+        });
+
+/*        Excel::selectSheets('imp1')->load('public/questions.xlsx', function ($reader) {
             $questions = $reader->all();
             foreach ($questions as $question) {
                 \App\Question::create($question->toArray());
@@ -47,7 +54,7 @@ class LoadController extends Controller
                 \App\Question::create($question->toArray());
             }
         });
-    }
+ */   }
     /**
      * Show the form for creating a new resource.
      *
