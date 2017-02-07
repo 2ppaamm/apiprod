af3f0cb03460a4593981e42043d822cbf670a1d2
Pamela Lim <pamelaliusm@gmail.com>
2017-02-07 20:20:06 +1000

    Gracia's error

--- app/Test.php
+++ app/Test.php
@@ -79,8 +79,7 @@ class Test extends Model
                 }              
 
             } elseif (!count($this->questions)) {           // not diagnostic, new test
-
-                $level = max(min(Level::find(7), Level::whereLevel(round($user->maxile_level/100)*100)->first()), Level::find(2));userlevel
+                $level = max(min(Level::find(7), Level::whereLevel(round($user->maxile_level/100)*100)->first()), Level::find(2));  // get userlevel
                 $user->testedTracks()->sync($level->tracks()->lists('id')->toArray(), false);
                 $tracks_to_test = count($user->tracksFailed) ? !$level->tracks->intersect($user->tracksFailed) ? $level->tracks->intersect($user->tracksFailed) : $user->tracksFailed : $level->tracks;                         // test failed tracks
                 if (count($tracks_to_test) < 3) {  
