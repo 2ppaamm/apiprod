b96751ee4744f9dddf6003f41997e417640fec95
Pamela Lim <pamelaliusm@gmail.com>
2018-05-04 04:45:15 +0800

    deploy for capstone

--- mooc/src/app/models/course.ts
+++
@@ -1,4 +0,0 @@
-export class Course {
-  constructor(public id:string, public course:string, public description:string, public image:string, public start_maxile_score:string, public end_maxile_score:string) {
-  }
-}
