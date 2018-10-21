04546c71943c70ba398887d796260167a48cee4e
Pamela Lim <pamelaliusm@gmail.com>
2018-10-22 00:45:51 +0800

    questions update images fixed

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -49,33 +49,39 @@ class QuestionController extends Controller
     public function store(Request $request)
     {
         $user = Auth::user();
-        $question = $request->all();
+        $question = $request->except(['question_image','answer0_image','answer1_image','answer2_image','answer3_image']);
         $question['user_id'] = $user->id;
-        $question = Question::create($question);
 
         if ($request->hasFile('question_image')) {
-            $file = $request->question_image->move(public_path('images\questions\question_image'), $question->id.'.png');            
-            $question->image = 'images/questions/question_image'.$question->id.'.png';
+            $q_image='q'.time().'.png';
+            $file = $request->question_image->move(public_path('images\questions\question_image'), $q_image);
+            $question['image'] = 'images/questions/question_image/'.$q_image;
+
         } 
 
         if ($request->hasFile('answer0_image')) {
-            $file = $request->answer0_image->move(public_path('images/questions/answers'), $question->id.'.answer0.png');            
-            $question->answer0_image = 'images/questions/answers'.$question->id.'.answer0.png';
+            $a0_image='a0'.time().'.png';
+            $file = $request->answer0_image->move(public_path('images/questions/answers'), $a0_image);            
+            $question['answer0_image'] = 'images/questions/answers'.$a0_image;
         }
 
         if ($request->hasFile('answer1_image')) {
-            $file = $request->answer1_image->move(public_path('images/questions/answers'), $question->id.'.answer1.png');            
-            $question->answer1_image = 'images/questions/answers'.$question->id.'.answer1.png';
+            $a1_image='a1'.time().'.png';
+            $file = $request->answer1_image->move(public_path('images/questions/answers'), $a1_image);            
+            $question['answer1_image'] = 'images/questions/answers'.$a1_image;
+        }
+        if ($request->hasFile('answer2_image')) {
+            $a2_image='a2'.time().'.png';
+            $file = $request->answer2_image->move(public_path('images/questions/answers'), $a2_image);            
+            $question['answer2_image'] = 'images/questions/answers'.$a2_image;
+        }
+        if ($request->hasFile('answer3_image')) {
+            $a3_image='a3'.time().'.png';
+            $file = $request->answer3_image->move(public_path('images/questions/answers'), $a3_image);            
+            $question['answer3_image'] = 'images/questions/answers'.$a3_image;
         }
 
-        if ($request->hasFile('answer2_image')) {
-            $file = $request->answer2_image->move(public_path('images/questions/answers'), $question->id.'.answer2.png');            
-            $question->answer2_image = 'images/questions/answers'.$question->id.'.answer2.png';
-        } 
-        if ($request->hasFile('answer3_image')) {
-            $file = $request->answer3_image->move(public_path('images/questions/answers'), $question->id.'.answer3.png');            
-            $question->answer3_image = 'images/questions/answers'.$question->id.'.answer3.png';
-        } 
+        $question = Question::create($question);
         return response()->json(['message' => 'Question correctly added', 'question'=>$question, 'code'=>201]);
     }
 
@@ -103,46 +109,52 @@ class QuestionController extends Controller
 //            return response()->json(['message'=> 'Access denied. You are not authorized to modify this question.', 'code'=>403],403);
 //        }
         $user = Auth::user();
+$user->is_admin = TRUE; //make everyone admin to delete at live
         if ($question->user_id != $user->id && !$user->is_admin) {
             return response()->json(['message'=> 'Access denied. You are not authorized to modify this question.', 'code'=>403],403);
         }
 
-        $question->fill($request->all())->save();
-
         if ($request->hasFile('question_image')) {
+            if (file_exists($question->question_image)) unlink($question->question_image);
+            $q_image = 'q'.time();
+            $question->question_image = '/images/questions/question_image/'.$q_image.'.png';
 
-            if (file_exists('images\questions\question_image/'.$question->id.'.png')) unlink('images/questions/question_image/'.$question->id.'.png'); 
-            $file = $request->question_image->move(public_path('images\questions\question_image/'), $question->id.'.png');
-
-           
-            $question->question_image = 'images/questions/question_image/'.$question->id.'.png';
-        } else $question->question_image = null; 
-
+            $file = $request->question_image->move(public_path('images/questions/question_image'), $q_image.'.png');
+        } 
 
         if ($request->hasFile('answer0_image')) {
-            $file = $request->answer0_image->move(public_path('images/questions/answers'), $question->id.'.answer0.png');            
+            if (file_exists($question->answer0_image)) unlink($question->answer0_image);
+            $a0_image = 'a0'.time();
+            $question['answer0_image'] = '/images/questions/answers/'.$a0_image.'.png';
 
-            if (file_exists('images\questions\answers'.$question->id.'answer0.png')) unlink('images/questions/answers'.$question->id.'.answer0.png'); 
-            
-            $question->answer0_image = 'images/questions/answers'.$question->id.'.answer0.png';
-        } else $question->answer0_image = null;
+            $file = $request->answer0_image->move(public_path('images/questions/answers'), $a0_image.'.png');
+        } 
 
         if ($request->hasFile('answer1_image')) {
-            $file = $request->answer1_image->move(public_path('images/questions/answers'), $question->id.'.answer1.png');            
-            if (file_exists('images\questions\answers'.$question->id.'.answer1.png')) unlink('images/questions/answers'.$question->id.'.answer1.png'); 
-            $question->answer1_image = 'images/questions/answers'.$question->id.'.answer1.png';
-        }  else $question->answer1_image = null;
+            if (file_exists($question->answer1_image)) unlink($question->answer1_image);
+            $a1_image = 'a1'.time();
+            $question['answer1_image'] = '/images/questions/answers/'.$a1_image.'.png';
+
+            $file = $request->answer1_image->move(public_path('images/questions/answers'), $a1_image.'.png');
+        } 
 
         if ($request->hasFile('answer2_image')) {
-            $file = $request->answer2_image->move(public_path('images/questions/answers'), $question->id.'.answer2.png');
-            if (file_exists('images\questions\answers'.$question->id.'.answer2.png')) unlink('images/questions/answers'.$question->id.'.answer2.png'); 
-            $question->answer2_image = 'images/questions/answers'.$question->id.'.answer2.png';
-        }  else $question->answer2_image = null; 
+            if (file_exists($question->answer2_image)) unlink($question->answer2_image);
+            $a2_image = 'a2'.time();
+            $question['answer2_image'] = '/images/questions/answers/'.$a2_image.'.png';
+
+            $file = $request->answer2_image->move(public_path('images/questions/answers'), $a2_image.'.png');
+        } 
 
         if ($request->hasFile('answer3_image')) {
-            $file = $request->answer3_image->move(public_path('images/questions/answers'), $question->id.'.answer3.png');            
-            if (file_exists('images\questions\answers'.$question->id.'.answer3.png')) unlink('images/questions/answers'.$question->id.'.answer3.png');            $question->answer3_image = 'images/questions/answers'.$question->id.'.answer3.png';
-        }  else $question->answer3_image = null;
+            if (file_exists($question->answer3_image)) unlink($question->answer3_image);
+            $a3_image = 'a3'.time();
+            $question['answer3_image'] = '/images/questions/answers/'.$a3_image.'.png';
+
+            $file = $request->answer3_image->move(public_path('images/questions/answers'), $a3_image.'.png');
+        } 
+
+        $question->fill($request->except(['question_image','answer0_image','answer1_image','answer2_image','answer3_image']))->save();
 
         return response()->json(['message'=>'Question updated','question' => $question, 'code'=>200], 200);
     }
