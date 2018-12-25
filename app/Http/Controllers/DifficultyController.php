b5729ef6e236d5e2133c6b364be6dc9d280a3dc1
andriodsean@gmail.com <sean.lim3@uq.net.au>
2018-12-25 23:44:54 +0800

    Enrolment CRUD

---
+++ app/Http/Controllers/DifficultyController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Difficulty;
+
+class DifficultyController extends Controller
+{
+    //
+}
